1a3a71de03819d42629d3912cb61e57c697b6089
Rob Hess <robwhess@gmail.com>
2018-05-14 10:15:14 -0700

    Add code from 5/14 lecture.

--- server.js
+++ server.js
@@ -4,10 +4,30 @@ function requestHandler(req, res) {
   console.log("== Got a request");
   console.log("  -- method:", req.method);
   console.log("  -- url:", req.url);
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+
+  if (req.url === '/style.css') {
+    res.write('body { font-family: Helvetica; }');
+    res.end();
+  } else {
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<link rel="stylesheet" href="style.css">')
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<h1>Hello world!!!!!!</h1>');
+    res.write('<p>You requested this URL:' + req.url + '</p>');
+    res.write('</body>');
+    res.write('</html>');
+  }
+
+  res.end();
 }
 
 var server = http.createServer(requestHandler);
 
-server.listen(8000, function () {
-  console.log("== Server is listening on port 8000");
+server.listen(3001, function () {
+  console.log("== Server is listening on port 3001");
 });
